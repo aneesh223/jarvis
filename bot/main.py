087ec1d476a4ac43082f79ca7e534c9dd4498286
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-09-20 20:47:36 -0400

    Added more stuff

--- bot/main.py
+++ bot/main.py
@@ -16,13 +16,13 @@ def who_is(query, session_id="general"):
                 return wikipedia.summary(new_query)
             except Exception:
                 pass
-    return "I don't know about "+query
+    return f"I don't know about {query}"
 template_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"chatbot","chatbot.template")
 chat=Chat(template_file_path)
 @bot.event
 async def on_ready():
     print(f"looged in as {bot.user.name}({bot.user.id})")
-@bot.command(pass_context = True, aliases=["@J.A.R.V.I.S#5351", "J.A.R.V.I.S.", "J.A.R.V.I.S", "JARVIS"])
+@bot.command(pass_context = True, aliases=["@J.A.R.V.I.S#5351", "J.A.R.V.I.S.", "J.A.R.V.I.S", "JARVIS", "!"])
 async def jarvis(ctx,*,message):
     result = chat.respond(message)
     if(len(result)<=2048):
@@ -35,11 +35,11 @@ async def jarvis(ctx,*,message):
         for num, item in enumerate(embedList, start = 1):
             if(num == 1):
                 embed = discord.Embed(title="J.A.R.V.I.S.", description = item, color = (0xF48D1))
-                embed.set_footer(text="Page {}".format(num))
+                embed.set_footer(text=f"Page {num}")
                 await ctx.send(embed = embed)
             else:
                 embed = discord.Embed(description = item, color = (0xF48D1))
-                embed.set_footer(text = "Page {}".format(num))
+                embed.set_footer(text = f"Page {num}")
                 await ctx.send(embed = embed)
 server.server()
 bot.run(TOKEN)
