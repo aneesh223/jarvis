4025904d3f0e6e373a5272eafe7789f1589b60a5
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-09-25 11:31:42 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,34 +1,15 @@
-import json, apiai, discord, asyncio, os, server
+import discord, wolframalpha, os
+from discord.ext import commands
+client = wolframalpha.Client(os.getenv("WA_TOKEN"))
+prefix = "$"
+bot = commands.Bot(command_prefix=prefix)
 
-client = discord.Client()
-token = os.getenv("DISCORD_TOKEN")
-client_token = os.getenv("DF_TOKEN")
-ai = apiai.ApiAI(client_token)
-
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'logged in as {client.user.name}({client.user.id})')
+    print(f"logged in as {bot.user.name}({bot.user.id})")
 
-@client.event
-async def on_message(message):
-    if not message.author.bot:
-        try:
-            if message.content.startswith("$"):
-                msg = message.content.split("$", 1)
-                request = ai.text_request()
-                request.query = msg
+@bot.command(aliases=["J.A.R.V.I.S.", "J.a.r.v.i.s.", "j.a.r.v.i.s.", "JARVIS", "Jarvis", "jarvis", ""])
+async def _(ctx, *, query):
+    await ctx.send(str(next(client.query(query).results).text))
 
-                response = json.loads(request.getresponse().read()) # Get the json from dialogflow
-                print(response)
-                
-                result = response['result'] # Get the actual plaintext reply
-                action = result.get('action') # Get the action (this can be helpful if you want to make the bot able to run commands when asked. For example, telling the bot to send help could make it DM a user with a help command)
-
-                await message.channel.send(response['result'], ['fulfillment'], ['speech']) # Send the message and tag the message author
-
-        except KeyError: # If the bot gets input dialogflow can't handle, it will raise a KeyError
-            await message.channel.send("KeyError!")
-
-server.server()
-client.run(token)
+bot.run(os.getenv("DISCORD_TOKEN"))
