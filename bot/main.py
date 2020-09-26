2438335a1b334eb391156742a9c533d1fd8b14b6
John Doe <johndo3@repl.email>
2020-09-26 22:33:10 +0000

    h

--- bot/main.py
+++ bot/main.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix=prefix)
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(f'{prefix} <ask anything> (VERY IMPORTANT YOU PUT SPACE BETWEEN $ AND COMMAND)'))
+    await bot.change_presence(activity=discord.Game(f'{prefix} <ask anything> (VERY IMPORTANT YOU PUT SPACE BETWEEN {prefix} AND COMMAND)'))
     print(f"logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command(aliases=["J.A.R.V.I.S.", "J.a.r.v.i.s.", "j.a.r.v.i.s.", "JARVIS", "Jarvis", "jarvis", ""])
