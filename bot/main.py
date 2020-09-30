9e4b9ee21867fb262ff3745dfd2ad55778bccc50
John Doe <johndo3@repl.email>
2020-09-30 16:35:17 +0000

    h

--- bot/main.py
+++ bot/main.py
@@ -3,15 +3,16 @@ from discord.ext import commands
 client = wolframalpha.Client(os.getenv("WA_TOKEN"))
 def get_prefix(client, message):
 
-    prefixes = ['j!', 'j?']    # sets the prefixes, u can keep it as an array of only 1 item if you need only one prefix
+    prefixes = ['j!']    # sets the prefixes, u can keep it as an array of only 1 item if you need only one prefix
 
     if not message.guild:
-        prefixes = ['']   # Only allow '==' as a prefix when in DMs, this is optional
+        prefixes = ['']
 
     # Allow users to @mention the bot instead of using a prefix when using a command. Also optional
     # Do `return prefixes` if u don't want to allow mentions instead of prefix.
-    return commands.when_mentioned or prefixes
-prefix = get_prefix
+    return commands.when_mentioned_or(*prefixes)(client, message)
+
+prefix = "j!"
 bot = commands.Bot(command_prefix=prefix, case_insensitive=True)
 
 @bot.event
@@ -19,7 +20,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(f'{prefix} <ask anything> (VERY IMPORTANT YOU PUT SPACE BETWEEN {prefix} AND COMMAND)'))
     print(f"logged in as {bot.user.name}({bot.user.id})")
 
-@bot.command(aliases=["J.A.R.V.I.S.", "J.a.r.v.i.s.", "j.a.r.v.i.s.", "JARVIS", "Jarvis", "jarvis", ""])
+@bot.command(aliases=[""])
 async def _(ctx, *, query):
     if str(next(client.query(query).results).text) == "My name is Wolfram|Alpha.":
       await ctx.send("My name is J.A.R.V.I.S.")
