7bacf9c6a6b281dcc07713dc5e6613c9764cbd96
John Doe <johndo3@repl.email>
2020-09-30 15:49:24 +0000

    g

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,18 @@
 import discord, wolframalpha, os, server
 from discord.ext import commands
 client = wolframalpha.Client(os.getenv("WA_TOKEN"))
-prefix = "j!"
-bot = commands.Bot(command_prefix=prefix)
+def get_prefix(client, message):
+
+    prefixes = ['j!', 'j?']    # sets the prefixes, u can keep it as an array of only 1 item if you need only one prefix
+
+    if not message.guild:
+        prefixes = ['']   # Only allow '==' as a prefix when in DMs, this is optional
+
+    # Allow users to @mention the bot instead of using a prefix when using a command. Also optional
+    # Do `return prefixes` if u don't want to allow mentions instead of prefix.
+    return commands.when_mentioned or (*prefixes)(client, message)
+prefix = get_prefix
+bot = commands.Bot(command_prefix=prefix, case_insensitive=True)
 
 @bot.event
 async def on_ready():
